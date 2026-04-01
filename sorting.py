c3b817ce38227807b9b6a72efa8be3ddba4a59b3
Michel <michelrostain@laplateforme.io>
2026-04-01 12:58:58 +0200

    Premiers fichiers

---
+++ sorting.py
@@ -0,0 +1,26 @@
+import random
+
+liste = []
+for k in range (101):
+    liste.append(random.randint(0, 100000))
+
+
+def tri_insertion(L):
+    N = len(L)
+    for n in range (1, N):
+        cle = L[n]
+        j = n-1
+        while j>=0 and L[j] > cle:
+            L [j+1] = L[j]
+            j = j-1
+        L[j+1] = cle
+
+def bubble_sort(arr):
+    n = len(arr)
+    switch = True
+    while switch == True :
+        switch = False
+        for i in range(0, n-1):
+            if arr[i] > arr[i+1]:
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+                switch = True
